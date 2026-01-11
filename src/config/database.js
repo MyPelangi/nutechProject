15936b91e2b262278a07b586487143cfed22db0e
MyPelangi <plngmawarni@gmail.com>
2026-01-11 19:03:50 +0700

    match db connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const msql = require('mysql2');
 
 const dbPool = msql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLSATABASE
 });
 
 module.exports = dbPool.promise();
