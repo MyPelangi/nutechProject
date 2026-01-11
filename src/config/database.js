bc8f3af86be88b0a846474d90655340fe5c5fe1f
MyPelangi <plngmawarni@gmail.com>
2026-01-11 18:04:18 +0700

    fix connection to DB

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const msql = require('mysql2');
 
 const dbPool = msql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 module.exports = dbPool.promise();
